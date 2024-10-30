d7dcb28207cb5102496f240ad1528844d61f0fc4
transfons <145350092+transfons@users.noreply.github.com>
2024-10-30 03:17:20 +0300

    Update Task.java

--- Task.java
+++ Task.java
@@ -2,42 +2,56 @@ import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
-public class Task {
+public class Main {
     public static void main(String[] args) {
         System.out.print("Введите два числа (диапазон): ");
-        Scanner in = new Scanner(System.in);
-        String str = in.nextLine();
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
 
-        int[] numArr = Arrays.stream(str.split(" ")).mapToInt(Integer::parseInt).toArray();
+        int[] numArr = InputValidator.parseAndValidateRange(input);
+
+        int result = RandomNumberGenerator.generate(numArr[0], numArr[1]);
+        System.out.println("Сгенерированное число = " + result + " в диапазоне от " + numArr[0] + " до " + numArr[1]);
+        }
+    }
+
+
+class InputValidator {
+    public static int[] parseAndValidateRange(String input) {
+        int[] numArr;
+        numArr = Arrays.stream(input.split(" ")).mapToInt(Integer::parseInt).toArray();
         int length = numArr.length;
 
-        if(length > 2) {
-            System.out.println("Неверно заданный диапазон: количество введенных чисел = "+length);
+        if (length > 2) {
+            System.out.println("Неверно заданный диапазон: количество введенных чисел = " + length);
             System.exit(0);
         }
         if (numArr[0] < 0) {
-            System.out.println("Число должно  быть положительным: "+numArr[0]);
+            System.out.println("Число должно  быть положительным: " + numArr[0]);
             System.exit(0);
         } else if (numArr[0] > 1000000) {
             System.out.println("Неверно заданный диапазон");
             System.exit(0);
         }
         if (numArr[1] < 1) {
-            System.out.println("Число должно  быть положительным: "+numArr[1]);
+            System.out.println("Число должно  быть положительным: " + numArr[1]);
             System.exit(0);
         } else if (numArr[1] > 1000000) {
             System.out.println("Неверно заданный диапазон");
             System.exit(0);
         }
-        if (numArr[0]>numArr[1]) {
-            System.out.println("Верхняя граница должна быть больше нижней: нижняя граница = "+numArr[0]+", верхняя граница = "+numArr[1]);
+        if (numArr[0] > numArr[1]) {
+            System.out.println("Верхняя граница должна быть больше нижней: нижняя граница = " + numArr[0] + ", верхняя граница = " + numArr[1]);
             System.exit(0);
         }
+        return numArr;
+    }
+}
 
-        int min = numArr[0];
-        int max = numArr[1];
+
+class RandomNumberGenerator {
+    public static int generate(int min, int max) {
         Random random = new Random();
-        int result = random.nextInt(max - min + 1) + min;
-        System.out.println("Сгенерированное число = "+result+" в диапазоне от "+numArr[0]+" до "+numArr[1]);
+        return random.nextInt(max - min + 1) + min;
     }
 }
